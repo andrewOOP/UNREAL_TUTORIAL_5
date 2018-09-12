1e6bf75f6ca393ef974dbc292cedb8213c7d254b
wellington <andrewOOP@users.noreply.github.com>
2018-09-11 22:48:45 -0500

    Initial commit

---
+++ Source/UNREAL_TUTORIAL_5/UNREAL_TUTORIAL_5.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UNREAL_TUTORIAL_5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UNREAL_TUTORIAL_5, "UNREAL_TUTORIAL_5" );
---
+++ Source/UNREAL_TUTORIAL_5/UNREAL_TUTORIAL_5GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UNREAL_TUTORIAL_5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_TUTORIAL_5_API AUNREAL_TUTORIAL_5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
