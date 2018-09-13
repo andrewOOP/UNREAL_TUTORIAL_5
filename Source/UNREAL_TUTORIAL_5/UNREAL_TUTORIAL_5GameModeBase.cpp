44e205bce7bdf2efe8ff74fe5e3667792c5e87b6
wellington <andrewOOP@users.noreply.github.com>
2018-09-12 22:45:21 -0500

    Final

--- Source/UNREAL_TUTORIAL_5/UNREAL_TUTORIAL_5GameModeBase.cpp
+++ Source/UNREAL_TUTORIAL_5/UNREAL_TUTORIAL_5GameModeBase.cpp
@@ -3,5 +3,25 @@
 #include "UNREAL_TUTORIAL_5GameModeBase.h"
 
 
+void AUNREAL_TUTORIAL_5GameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+}
 
-
+void AUNREAL_TUTORIAL_5GameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
--- Source/UNREAL_TUTORIAL_5/UNREAL_TUTORIAL_5GameModeBase.h
+++ Source/UNREAL_TUTORIAL_5/UNREAL_TUTORIAL_5GameModeBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Blueprint/UserWidget.h"
 #include "UNREAL_TUTORIAL_5GameModeBase.generated.h"
 
 /**
@@ -15,6 +16,21 @@ class UNREAL_TUTORIAL_5_API AUNREAL_TUTORIAL_5GameModeBase : public AGameModeBas
 	GENERATED_BODY()
 	
 	
-	
+public:
+	/** Remove the current menu widget and create a new one from the specified class, if provided. */
+	UFUNCTION(BlueprintCallable, Category = "UMG Game")
+		void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+protected:
+	/** Called when the game starts. */
+	virtual void BeginPlay() override;
+
+	/** The widget class we will use as our menu when the game starts. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UMG Game")
+		TSubclassOf<UUserWidget> StartingWidgetClass;
+
+	/** The widget instance that we are using as our menu. */
+	UPROPERTY()
+		UUserWidget* CurrentWidget;
 	
 };
